d60225ab3bc62b4ab851dc724c9eff6b252bc4d2
kiku ishimoto <kikuish@gmail.com>
2020-05-20 01:24:43 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  extend
-FancyDance::ClassMethods
-  include
-FancyDance::InstanceMethods
-
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
