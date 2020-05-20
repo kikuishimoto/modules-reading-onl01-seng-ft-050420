051319770f400a8559506143b48b2096b803e6be
kiku ishimoto <kikuish@gmail.com>
2020-05-20 01:18:29 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,23 +2,23 @@ module FancyDance
   module InstanceMethods
     def twirl
       "I'm twirling!"
-    end 
+    end
     def jump
       "Look how high I'm jumping!"
-    end 
+    end
 
     def pirouette
       "I'm doing a pirouette"
-    end 
+    end
 
-    def take_a_bow 
+    def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
-    end 
-  end 
+    end
+  end
 
   module ClassMethods
     def metadata
       "This class produces objects that love to dance."
-    end 
-  end 
+    end
+  end
 end 
