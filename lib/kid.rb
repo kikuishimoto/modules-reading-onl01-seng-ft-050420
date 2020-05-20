bbc0a992012c5e210787413441f9d6ad81866341
kiku ishimoto <kikuish@gmail.com>
2020-05-20 01:31:28 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
-  extend FancyDance::ClassMethods
+  include Dance
+  extend MetaDancing
   include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
